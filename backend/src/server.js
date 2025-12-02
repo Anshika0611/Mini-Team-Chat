00612b24416305b9ebad0a11ad00ce0137338584
Anshika Awasthi <114733427+Anshika0611@users.noreply.github.com>
2025-12-02 22:21:43 +0530

    db connected & env.js completed

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,8 @@
 import express from 'express'
 import {ENV} from './config/env.js'
+import {connectDB} from './config/db.js'
 const app=express()
+
 
 app.get('/',(req,res)=>{
     res.send('hey anshika')
@@ -8,4 +10,5 @@ app.get('/',(req,res)=>{
 
 app.listen(ENV.PORT,()=>{
     console.log('Server is running on port:',ENV.PORT);
+    connectDB()
 })
