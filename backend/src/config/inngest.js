e22d0ac2fc32b37dd0dd13986d0cd43984ece052
Anshika Awasthi <114733427+Anshika0611@users.noreply.github.com>
2025-12-04 11:39:57 +0530

    tiny changes in inngest.js

--- backend/src/config/inngest.js
+++ backend/src/config/inngest.js
@@ -26,6 +26,7 @@ const deleteUser = inngest.createFunction(
   { id: "delete-user-from-db" },
   { event:'clerk/user.deleted'},
   async({event})=>{
+     await connectDB();
     const {id}=event.data
     await User.deleteOne({clerkId:id})
   }
--- backend/src/server.js
+++ backend/src/server.js
@@ -2,7 +2,7 @@ import express from "express";
 import { ENV } from "./config/env.js";
 import { connectDB } from "./config/db.js";
 import { clerkMiddleware } from "@clerk/express";
-import { functions } from "./config/inngest.js";
+import { inngest,functions } from "./config/inngest.js";
 import { serve } from "inngest/express";
 const app = express();
 
@@ -10,14 +10,14 @@ app.use(express.json());
 
 app.use("/api/inngest", serve({ client: inngest, functions }));
 app.use(clerkMiddleware());
+
 app.get("/", (req, res) => {
-  functions.syncUser();
   res.send("hey anshika");
 });
 const startServer = async () => {
   try {
     await connectDB();
-    if (ENV.NODE_ENV !== Production) {
+    if (ENV.NODE_ENV !== 'production') {
       app.listen(ENV.PORT, () => {
         console.log("Server is running on port:", ENV.PORT);
       });
