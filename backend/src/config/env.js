a17e5b98f848a4a36d3ee1c1088dbae1e98c28fc
Anshika Awasthi <114733427+Anshika0611@users.noreply.github.com>
2025-12-04 12:59:55 +0530

    tiny env change

--- backend/src/config/env.js
+++ backend/src/config/env.js
@@ -2,7 +2,7 @@ import dotenv from "dotenv";
 dotenv.config();
 
 export const ENV = {
-  PORT: process.env.PORT || 5001,
+  PORT: process.env.PORT || 5000,
   MONGO_URI: process.env.MONGO_URI,
   NODE_ENV: process.env.NODE_ENV,
   CLERK_PUBLISHABLE_KEY:process.env.CLERK_PUBLISHABLE_KEY,
