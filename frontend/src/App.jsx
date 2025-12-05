81c5856c6211370cf56cd10cd935d6214a55eef5
Anshika Awasthi <114733427+Anshika0611@users.noreply.github.com>
2025-12-05 12:47:43 +0530

    useStreamChat & tailwind added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,28 +1,49 @@
 import React from "react";
-import { SignedIn, SignedOut } from "@clerk/clerk-react";
+import { SignedIn, SignedOut, useAuth } from "@clerk/clerk-react";
 import { Navigate, Route, Routes } from "react-router";
 import HomePage from "./Pages/HomePage";
 import AuthPage from "./Pages/AuthPage";
+import CallPage from "./Pages/CallPage";
 import toast from "react-hot-toast";
 import * as Sentry from "@sentry/react";
 
 const SentryRoutes = Sentry.withSentryReactRouterV7Routing(Routes);
 
 const App = () => {
+  const { isSignedIn, isLoaded } = useAuth();
+  if (!isLoaded) return null;
   return (
     <>
-      <SignedIn>
-        <SentryRoutes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/auth" element={<Navigate to={"/"} replace />} />
-        </SentryRoutes>
-      </SignedIn>
-      <SignedOut>
-        <SentryRoutes>
-          <Route path="/auth" element={<AuthPage />} />
-          <Route path="*" element={<Navigate to={"/auth"} replace />} />
-        </SentryRoutes>
-      </SignedOut>
+      <SentryRoutes>
+        <Route
+          path="/"
+          element={
+            isSignedIn ? <HomePage /> : <Navigate to={"/auth"} replace />
+          }
+        />
+        <Route
+          path="/auth"
+          element={!isSignedIn ? <AuthPage /> : <Navigate to={"/"} replace />}
+        />
+
+        <Route
+          path="/call/:id"
+          element={
+            isSignedIn ? <CallPage /> : <Navigate to={"/auth"} replace />
+          }
+        />
+
+        <Route
+          path="*"
+          element={
+            isSignedIn ? (
+              <Navigate to={"/"} replace />
+            ) : (
+              <Navigate to={"/auth"} replace />
+            )
+          }
+        />
+      </SentryRoutes>
     </>
   );
 };
